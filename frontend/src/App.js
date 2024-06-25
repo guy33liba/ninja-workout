215079ebb5825105b7ff146650ca8eda3096d930
Guy Liba <guy33liba@gmail.com>
2024-06-25 16:21:59 +0200

    v\z

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route, Route } from "react-router-dom"
 import "./App.css"
-import Workout from "./Workout"
-
+import Workout from "./pages/Workout"
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
 function App() {
   return (
     <div className="App">
-      <Workout />
+      <BrowserRouter>
+      <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/workout" element={<Workout />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
