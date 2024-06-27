899d467ef6722b2910efc79a703c7c274e004c41
Guy Liba <guy33liba@gmail.com>
2024-06-27 18:45:31 +0200

    kj

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -9,7 +9,7 @@ const signupUser = async (req, res) => {
     const user = await User.signup(email, password)
     res.status(200).json({ email, user })
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message })//
   }
 }
 
