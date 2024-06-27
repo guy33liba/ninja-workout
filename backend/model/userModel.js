799bfa15f67afdee40ebd307d7efa4250a56faf4
Guy Liba <guy33liba@gmail.com>
2024-06-27 17:59:33 +0200

    fdsa

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose"
 import bcrypt from "bcrypt"
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 })
 
 // static signup method
-userSchema.statics.signup = async (email, password) => {
-  const exists = await User.findOne({ email })
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email })
 
   if (exists) {
     throw new Error("Email Already in Use")
   }
+
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
   const user = await this.create({ email, password: hash })
   return user
 }
+
 const User = mongoose.model("User", userSchema)
-User.signup()
+
 export default User
