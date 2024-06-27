c9d3f0b05a7dcef841e683ada83411a6a8bdb920
guy33liba <guy33liba@gmail.com>
2024-06-27 22:24:22 +0300

    fad

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -2,7 +2,7 @@ import User from "../model/userModel.js"
 import jwt from "jsonwebtoken"
 
 const createToken = (_id) => {
-  const token = jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" })
+  const token = jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" })
   return token
 }
 const loginUser = async (req, res) => {
@@ -13,7 +13,10 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body
   try {
     const user = await User.signup(email, password)
-    res.status(200).json({ email, user })
+
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
   } catch (error) {
     res.status(400).json({ error: error.message }) //
   }
