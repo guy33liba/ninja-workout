9c16cca51a64d6d8cc6e0e8782635c5ae52e7c4e
Guy Liba <guy33liba@gmail.com>
2024-06-27 18:15:20 +0200

    dfas

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,15 +1,17 @@
 import User from "../model/userModel.js"
+
 const loginUser = async (req, res) => {
   res.json({ mssg: "login user" })
 }
 const signupUser = async (req, res) => {
   res.json({ mssg: "signup  user" })
   const { email, password } = req.body
-
   try {
     const user = await User.signup(email, password)
     res.status(200).json({ email, user })
-  } catch (error) {}
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 export { signupUser, loginUser }
