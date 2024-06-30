4c80865c26b6f556c34450c11020601add35a2d8
Guy Liba <guy33liba@gmail.com>
2024-06-30 11:49:44 +0200

    fdas

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,11 +1,14 @@
 import jwt from "jsonwebtoken"
 import { User } from "../model/userModel.js"
+
 const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers
+
   if (!authorization) {
     return res.status(401).json({ error: "Authorization token Required" })
   }
   const token = authorization.split(" ")[1]
+
   try {
     const { _id } = jwt.verify(token, process.env.JWT_SECRET)
     req.user = await User.findOne({ _id }).select("_id")
@@ -15,4 +18,5 @@ const requireAuth = async (req, res, next) => {
     res.status(401).json({ error: "Request Is Not Authorized" })
   }
 }
+
 export default requireAuth
