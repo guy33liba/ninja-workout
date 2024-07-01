a1c03038d74b0324cefa556c09f31f0d13686565
Guy Liba <guy33liba@gmail.com>
2024-07-01 16:03:34 +0200

    fdas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import "./App.css"
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import { useAuthContext } from "./hooks/useAuthContext"
+
 function App() {
+  const { user } = useAuthContext()
+  const navigate = useNavigate()
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={user ? <Home /> : navigate("/login")} />
+            <Route path="/login" element={!user ? <Login /> : navigate("/")} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
