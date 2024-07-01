0c7bf777d2797ce2f18ca7abdf6496902ea35a1a
Guy Liba <guy33liba@gmail.com>
2024-07-01 16:20:53 +0200

    sadf

--- backend/model/workoutSchema.js
+++ backend/model/workoutSchema.js
@@ -5,8 +5,9 @@ const workoutSchema = new mongoose.Schema(
     title: { type: String, required: true },
     reps: { type: Number, required: true },
     load: { type: Number, required: true },
+    user_id: { type: String, required: true },
   },
-  { timestamps: true }
+  { timestamps: true },
 )
 const Workout = mongoose.model("Workout", workoutSchema)
 
