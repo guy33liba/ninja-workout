576be8aaa210046b54455488ef7869e37d5f070a
Guy Liba <guy33liba@gmail.com>
2024-07-01 16:04:00 +0200

    navigate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={user ? <Home /> : navigate("/login")} />
             <Route path="/login" element={!user ? <Login /> : navigate("/")} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/signup" element={!user ? <Signup /> : navigate("/")} />
           </Routes>
         </div>
       </BrowserRouter>
